3a6538bf9f057a166d5e63d175977159bdb94bd5
lodin <LouieDinh@gmail.com>
2013-07-09 17:09:40 -0700

    Add Heroku installation

--- setup.sh
+++ setup.sh
@@ -16,6 +16,10 @@ nvm use v0.10.12
 # http://jshint.com/
 npm install -g jshint
 
+# Install Heroku
+wget -qO- https://toolbelt.heroku.com/install-ubuntu.sh | sh
+
+
 # Install rlwrap to provide libreadline features with node
 # See: http://nodejs.org/api/repl.html#repl_repl
 sudo apt-get install -y rlwrap
